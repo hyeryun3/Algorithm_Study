51c887461452a48dad0389d40f032891d26b7b6b
hyeryun3 <hryun3@gmail.com>
2022-03-18 00:11:05 +0900

    .

--- study/level1/KakaoColoring.java
+++ study/level1/KakaoColoring.java
@@ -2,6 +2,9 @@ package study.level1;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Queue;
+
+import org.w3c.dom.Node;
 
 public class KakaoColoring {
 
@@ -18,9 +21,15 @@ public class KakaoColoring {
 
 		};
 
-		solution(m, n, picture);
+		solution2(m, n, picture);
 	}
 
+	
+	public static int[] solution2(int m, int n, int[][] picture) {
+		System.out.println("아 어려워.....");
+		
+		return new int[2];
+	}
 	public static int[] solution(int m, int n, int[][] picture) {
 		int numberOfArea = 0;
 		int maxSizeOfOneArea = 0;
@@ -46,38 +55,29 @@ public class KakaoColoring {
 					}
 				}
 			}
-			
 		}
 
 		System.out.println(numberOfArea);
 		
 		int tmp=1;
 		
+		
 		for (int i = 0; i < picture.length; i++) {
 			for (int j = 0; j < picture[i].length; j++) {
 				if (picture[i][j] != 0) {
-					if(j + 1 < picture[i].length && picture[i][j] == picture[i][j+1]) {
-						tmp++;
-						while(true) {
-							if(picture[i][j] != picture[i+1][j]) {
-								i=j;
-								break;
-							}else {
-								tmp++;
-								i++;
-								System.out.println("["+i+"]["+j+"] = "+tmp );
-							}
-						}
+					if (j + 1 < picture[i].length && picture[i][j] == picture[i][j + 1]) {
+						numberOfArea--;
+					}
+					if (i + 1 < picture.length && picture[i][j] == picture[i + 1][j]) {
+						numberOfArea--;
 					}
 				}
 			}
-			if(i + 1 < picture.length && picture[i]!=picture[i+1]) {
-				if (tmp>maxSizeOfOneArea) {
-					maxSizeOfOneArea=tmp;
-					tmp=0;
-				}
-			}
 		}
+		
+		
+		
+		
 		System.out.println(maxSizeOfOneArea);
 
 		answer[0] = numberOfArea;
