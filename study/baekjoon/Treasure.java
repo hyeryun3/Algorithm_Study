3ef784db8f0dd51380dc0eecce44ecb313e80fe3
hyeryun3 <hryun3@gmail.com>
2022-03-22 23:50:44 +0900

    ..

---
+++ study/baekjoon/Treasure.java
@@ -0,0 +1,5 @@
+package study.baekjoon;
+
+public class Treasure {
+
+}
--- study/level2/KakaoColoring.java
+++ study/level2/KakaoColoring.java
@@ -20,34 +20,7 @@ public class KakaoColoring {
 //		BFS( QUEUE 이용 )
 		solution(m, n, picture);
 	}
-	public static int bfs(int m,int n, int x, int y, int[][] picture) {
-		
-		int cnt =1;
-		Queue<Integer> que = new LinkedList<Integer>();
-		
-		int[] moveX = {1,0,-1,0};
-		int[] moveY= {0,1,0,-1};
-		boolean[][] visit = new boolean[picture.length][picture[0].length]; 
-
-		int tmp1 = 0;
-		int tmp2 = 0;
-		for(int i=0; i<4; i++) {
-			
-			if( tmp1 >=0 && tmp2 >=0 && tmp1<picture.length && tmp2<picture[0].length) {
-				
-				que.add(picture[tmp1][tmp2]);
-				visit[tmp1][tmp2]=true;
-				cnt++;
-				System.out.println(que);
-			}
-		}
-		
-		
-		
-		
-		return cnt;
-	}
-
+	
 	public static int[] solution(int m, int n, int[][] picture) {
 		int numberOfArea = 0;
 		int maxSizeOfOneArea = 0;
@@ -75,73 +48,63 @@ public class KakaoColoring {
 			}
 		}
 
-		System.out.println(numberOfArea);
-		
 		
 		Queue<Integer> que = new LinkedList<Integer>();
-		int cnt=0;
 		while(true) {
 			for(int i=0; i<m; i++) {
 				for(int j=0; j<n; j++) {
 					if(picture[i][j] > 0 && que.isEmpty()) {
+						System.out.println("Que삽입");
 						que.add(picture[i][j]);
 						picture[i][j]=-1;
-						System.out.println("["+i+"]["+j+"] = " + que.size()); 
+						break;
 					}
 				}
 			}
-			
-			for(int i=0; i<m; i++) {
-				for(int j=0; j<n; j++) {
-					if(picture[i][j] > 0 && que.isEmpty()) {
-						que.add(picture[i][j]);
-						picture[i][j]=-1;
-						System.out.println("["+i+"]["+j+"] = " + que.size()); 
-					}
-					
-					if(i>0) {
-						if(que.peek() == picture[i-1][j]) {
-							que.add(picture[i-1][j]);
-							picture[i-1][j]=-1;
-							System.out.println("["+i+"]["+j+"] = " + que.size());
-							cnt++;
-						}
-					}
-					if(j<n-1) {
-						if(que.peek() == picture[i][j+1]) {
-							que.add(picture[i][j+1]);
-							picture[i][j+1]=-1;
-							System.out.println("["+i+"]["+j+"] = " + que.size());
-							cnt++;
-						}
-					}
-					if(i<m-1) {
-						if(que.peek() == picture[i+1][j]) {
-							que.add(picture[i+1][j]);
-							picture[i+1][j]=-1;
-							System.out.println("["+i+"]["+j+"] = " + que.size());
-							cnt++;
-						}
-					}
-					if(j>0) {
-						if(que.peek() == picture[i][j-1]) {
-							que.add(picture[i][j-1]);
-							picture[i][j-1]=-1;
-							System.out.println("["+i+"]["+j+"] = " + que.size());
-							cnt++;
-						}
-					}
-					
-				}
-			}
-			System.out.println(que.size());
-			maxSizeOfOneArea = maxSizeOfOneArea < que.size() ? que.size() : maxSizeOfOneArea;
 			
 			if(que.isEmpty()) {
 				break;
 			}
-			que.clear();
 			
+			System.out.println("que는? " + que);
+			int cnt=0;
+			for(int j=0; j<n; j++) {
+				for(int i=0; i<m; i++) {
+					if(picture[i][j] != 0) {
+						if(i>0 && que.peek() == picture[i-1][j]) {
+							que.add(picture[i-1][j]);
+							System.out.println("["+i+"]["+j+"] = " + picture[i-1][j]);
+							picture[i-1][j]=-1;
+						}else if(i<m-1 && que.peek() == picture[i+1][j] ) {
+							que.add(picture[i+1][j]);
+							System.out.println("["+i+"]["+j+"] = "  + picture[i+1][j]);
+							picture[i+1][j]=-1;
+						}else if(j<n-1 && que.peek() == picture[i][j+1] ) {
+							que.add(picture[i][j+1]);
+							System.out.println("["+i+"]["+j+"] = "  + picture[i][j+1]);
+							picture[i][j+1]=-1;
+						}else if(j>0 && que.peek() == picture[i][j-1] ) {
+							que.add(picture[i][j-1]);
+							System.out.println("["+i+"]["+j+"] = "  + picture[i][j-1]);
+							picture[i][j-1]=-1;
+						}else if(picture[i][j]== -1){
+							continue;
+						}else {
+							cnt++;
+						}
+					}
+				}
+				if(cnt>=2) {
+					maxSizeOfOneArea = maxSizeOfOneArea < que.size() ? que.size() : maxSizeOfOneArea;
+					que.clear();
+				}
+				if(cnt>=2) {
+					cnt=0;
+					break;
+				}
+			}
+			
+			System.out.println("==========");
 			
 		}
 		
