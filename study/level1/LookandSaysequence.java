172472a0e5b2d9321f23f0034b82c146278ed0e0
hyeryun3 <hryun3@gmail.com>
2022-04-10 17:46:36 +0900

    개미수열

--- study/level1/LookandSaysequence.java
+++ study/level1/LookandSaysequence.java
@@ -18,9 +18,9 @@ public class LookandSaysequence {
 		ans[0] = "1";
 
 		for (int i = 1; i < num; i++) {
-			ans[i] = test(ans[i-1]);
+			ans[i] = test(ans[i - 1]);
 		}
-		System.out.println(ans[num-1]);
+		System.out.println(ans[num - 1]);
 	}
 
 	public static String test(String str) {
@@ -28,23 +28,49 @@ public class LookandSaysequence {
 
 		int cnt = 1;
 		char[] anschar = str.toCharArray();
-		
+
 		answer = String.valueOf(anschar[0]);
-		
+		Queue<Character> que = new LinkedList<Character>();
+
+		for (int i = 0; i < str.length(); i++) {
+			que.add(anschar[i]);
+		}
+
 		if (str.length() == 1) {
 			answer += cnt;
-		} else {
-			for (int i = 0; i < str.length() - 1; i++) {
-				if(anschar[i]==anschar[i+1]) {
-					cnt++;
+		}
+		char temp = que.poll();
+
+		while (!que.isEmpty()) {
+			if (temp == que.peek()) {
+				cnt++;
+				temp = que.poll();
+				if (que.isEmpty()) {
+					answer += cnt;
+					cnt = 1;
+				}else if( temp != que.peek()) {
 					answer+=cnt;
-				}else {
-					answer += cnt + String.valueOf(anschar[i+1]);
-					cnt=1;
-					answer +=cnt;
+					temp=que.poll();
+					answer+=temp;
+					cnt = 1;
+				}
+			} else {
+				answer+=cnt;
+				temp = que.poll();
+				answer+=temp;
+				if(que.isEmpty()) {
+					answer+=cnt;
 				}
 			}
+
 		}
+
+		/*
+		 * if (str.length() == 1) { answer += cnt; } else { for (int i = 0; i <
+		 * str.length() - 1; i++) { if(anschar[i]==anschar[i+1]) { cnt++; answer+=cnt;
+		 * }else { answer += cnt + String.valueOf(anschar[i+1]); cnt=1; answer +=cnt; }
+		 * } }
+		 */
 		return answer;
 	}
 }
