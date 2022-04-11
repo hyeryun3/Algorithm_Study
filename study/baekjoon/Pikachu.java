9645fab77ce4666d776fbcd09551449300b2e47c
hyeryun3 <hryun3@gmail.com>
2022-04-11 23:56:46 +0900

    백준 14405_피카추

---
+++ study/baekjoon/Pikachu.java
@@ -0,0 +1,22 @@
+package study.baekjoon;
+
+import java.util.Scanner;
+
+public class Pikachu {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String s = scanner.next();
+		String[] pikachu = {"pi","ka","chu"};
+		
+		for(int i=0; i<s.length();i++) {
+			s=s.replaceAll(pikachu[i], "");
+		}
+
+		if(s=="") {
+			System.out.println("YES");
+		}else {
+			System.out.println("NO");
+		}
+	}
+}
