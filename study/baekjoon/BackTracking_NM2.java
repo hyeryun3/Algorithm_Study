ccd487ba612510121c11d6ea5d8aaaa84aa87206
hyeryun3 <hryun3@gmail.com>
2022-05-01 23:37:38 +0900

    백준_15651

--- study/baekjoon/BackTracking_NM2.java
+++ study/baekjoon/BackTracking_NM2.java
@@ -1,8 +1,50 @@
 package study.baekjoon;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+
 public class BackTracking_NM2 {
 
-	public static void main(String[] args) {
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	public static int[] arr;
+ 
+	public static void main(String[] args) throws IOException {
+ 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+ 
+		int n= Integer.parseInt(st.nextToken());
+		int m= Integer.parseInt(st.nextToken());
+ 
+		arr = new int[m];
+        
+		backtracking(n,m,0);
 		
+		br.close();
+		
+		bw.flush();
+		bw.close();
 	}
+ 
+	public static void backtracking(int n, int m, int depth) throws IOException {
+		if (depth == m) {
+			for (int i = 0; i < m; i++) {
+				bw.write(String.valueOf(arr[i] + " "));
+			}
+			bw.newLine();
+			return;
+		}
+ 
+		for (int i = 1; i <= n; i++) {
+			arr[depth] = i;
+			backtracking(n,m,depth + 1);
+		}
+
+	}
+ 
 }
