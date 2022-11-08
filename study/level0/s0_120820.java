33dbaf1ceed66de2b7249d24e6a80510ba2e5679
hyeryun3 <hryun3@gmail.com>
2022-11-08 14:06:24 +0900

    프로그래머스_120820

---
+++ study/level0/s0_120820.java
@@ -0,0 +1,12 @@
+package study.level0;
+
+public class s0_120820 {
+    public static void main(String[] args) {
+        int age=40;
+        System.out.println(solution(age));
+    }
+    public static int solution(int age) {
+        int answer = 2023-age;
+        return answer;
+    }
+}
