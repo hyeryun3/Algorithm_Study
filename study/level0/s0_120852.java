9f8009a79dd9c03c0ba8ed804cf2450732fdda53
hyeryun3 <hryun3@gmail.com>
2022-11-15 16:01:15 +0900

    프로그래머스_120852

---
+++ study/level0/s0_120852.java
@@ -0,0 +1,32 @@
+package study.level0;
+
+import java.util.*;
+
+public class s0_120852 {
+    public static void main(String[] args) {
+        int n = 12;
+        System.out.println(solution(n));
+    }
+    public static int[] solution(int n) {
+        HashSet<Integer> set = new HashSet<>();
+        int i = 2;
+        int cnt=0;
+        while(n>=i){
+            if(n%i==0){
+                set.add(i);
+                n=n/i;
+            }else{
+                i++;
+            }
+        }
+        int[] answer = new int[set.size()];
+        int size=0;
+        TreeSet<Integer> ts = new TreeSet<>();
+        ts.addAll(set);
+        for(int a:ts){
+            answer[size++]=a;
+        }
+
+        return answer;
+    }
+}
