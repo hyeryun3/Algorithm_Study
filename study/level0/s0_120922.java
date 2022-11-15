914cd24c9dc5945c291572fb89fc16b6ab640b72
hyeryun3 <hryun3@gmail.com>
2022-11-15 16:42:58 +0900

    프로그래머스_120922

---
+++ study/level0/s0_120922.java
@@ -0,0 +1,12 @@
+package study.level0;
+
+public class s0_120922 {
+    public static void main(String[] args) {
+        int M=2, N=2;
+        System.out.println(solution(M,N));
+    }
+    public static int solution(int M, int N) {
+        int answer = M*N-1;
+        return answer;
+    }
+}
